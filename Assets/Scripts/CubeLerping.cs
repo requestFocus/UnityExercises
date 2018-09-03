8b563d34c45b01b0217e447a543892b4d149cb50
requestFocus <1983wasnotaleapyear@gmail.com>
2018-09-03 21:26:45 +0200

    commit

---
+++ Assets/Scripts/CubeLerping.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeLerping : MonoBehaviour
+{
+    public Transform Target;
+
+    private const float _timeDuration = 1;
+    private float _timeFraction = 0;
+    private Vector3 _startPosition;
+    private Vector3 _endPosition;
+
+    private Vector3 _relativePosition;
+
+    private Quaternion _startRotation;
+    private Quaternion _endRotation;
+    private Quaternion _targetRotation;
+
+    private void Start()
+    {
+        //==== obrot o 50st, a jesli to wiecej niż aktualna roznica miedzy obiektami to dostraja do Target.rotation
+        //transform.rotation = Quaternion.RotateTowards(transform.rotation, Target.rotation, 50);
+
+        _startPosition = transform.position;
+        _endPosition = Target.transform.position;
+
+        _startRotation = transform.rotation;
+        _endRotation = Target.transform.rotation;
+
+        _relativePosition = transform.position - Target.transform.position;
+
+        //==== tworzy quaternion wg wektora _relativePosition i opcjonalnego wskazania kierunku dla osi Y (czyli UP), można wskazać, by rotacja wskazywała na rotację targetu!
+        //_targetRotation = Quaternion.LookRotation(_relativePosition, Target.transform.up);
+
+        //==== jeden raz obraca transform wokół osi z.back wzgledem punktu -2, 0, 0 o 36st
+        //transform.RotateAround(new Vector3(-2, 0, 0), Vector3.back, 36);
+
+        //==== przestawia rotację obiektu biorąc orientacje Vector3.right i ustawiając ją na transform.forward (dobrze to widać na Scenie/Local)
+        //transform.rotation = Quaternion.FromToRotation(Vector3.right, transform.forward);       
+    }
+
+    private void Update()
+    {
+        transform.position = Vector3.Lerp(_startPosition, _endPosition, _timeFraction * 0.5f);
+
+        //==== liniowa interpolacja od rotacji Start do Target po czasie timeFraction (0 ... 1)
+        //transform.rotation = Quaternion.Lerp(_startRotation, _endRotation, _timeFraction * 0.5f);
+
+        //==== liniowa interpolacja od rotacji Start do Quaternion.LookRotation po czasie timeFraction (0 ... 1)
+        //transform.rotation = Quaternion.Lerp(_startRotation, _targetRotation, _timeFraction * 0.5f);
+
+        //==== rotacja wzgledem x.left
+        //transform.Rotate(Vector3.left);
+
+        //==== obrot ciągły o 30st na sekunde, po 12 sekundach rotacja zrobi pełne koło
+        //transform.RotateAround(new Vector3(-2, 0, 0), Vector3.back, 30 * Time.deltaTime);     
+
+        //==== podąża za targetem, ustawia się do niego wg wskazanego kierunku
+        //transform.LookAt(Target, Vector3.left);                                               
+
+        //==== ustawia rotację transforma względem targetu
+        //transform.rotation = Quaternion.RotateTowards(transform.rotation, Target.rotation, 30 * Time.deltaTime);
+
+        //==== fraction calculations
+        _timeFraction += (Time.deltaTime / _timeDuration);
+    }
+}
+
---
+++ Assets/Scripts/RateUs.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class RateUs : MonoBehaviour
+{
+    private const float _duration = 1;
+    private float _timeFraction1 = 0;
+    private float _timeFraction2 = 0;
+    private Vector3 _startPosBg;
+    private Vector3 _middlePosBg;
+    private Vector3 _endPosBg;
+    private Vector3 _startPosText1;
+    private Vector3 _middlePosText1;
+    private Vector3 _endPosText1;
+    private Vector3 _startPosText2;
+    private Vector3 _middlePosText2;
+    private Vector3 _endPosText2;
+
+    public Button RateUsButton;
+    public Button RateLaterButton;
+
+    private bool _isHidden = false;
+
+    private void Start()
+    {
+        _startPosBg = transform.position;
+        _middlePosBg = new Vector3(0, 0, 0);
+        _endPosBg = new Vector3(15, 0, 0);
+
+        _startPosText1 = new Vector3(-250, 350, 0);
+        _middlePosText1 = new Vector3(450, 350, 0);
+        _endPosText1 = new Vector3(1150, 350, 0);
+
+        _startPosText2 = new Vector3(-150, 350, 0);
+        _middlePosText2 = new Vector3(550, 350, 0);
+        _endPosText2 = new Vector3(1250, 350, 0);
+
+        RateUsButton.onClick.AddListener(GoToStore);
+        RateLaterButton.onClick.AddListener(HideNotification);
+
+        _isHidden = true;
+    }
+
+    private void Update()
+    {
+        if (_isHidden)
+            RollWindow();
+        else
+            PostponeRating();
+    }
+
+    private void RollWindow()
+    {
+        transform.position = Vector3.Lerp(_startPosBg, _middlePosBg, _timeFraction1);
+        RateUsButton.transform.position = Vector3.Lerp(_startPosText1, _middlePosText1, _timeFraction1);
+        RateLaterButton.transform.position = Vector3.Lerp(_startPosText2, _middlePosText2, _timeFraction1);
+        _timeFraction1 += (Time.deltaTime / _duration);
+
+        _isHidden = true;
+    }
+
+    private void GoToStore()
+    {
+        Application.OpenURL("http://www.google.com");
+        // Application.OpenURL ("market://details?id=" + Application.productName);
+    }
+
+    private void PostponeRating()
+    {
+        transform.position = Vector3.Lerp(_middlePosBg, _endPosBg, _timeFraction2);
+        RateUsButton.transform.position = Vector3.Lerp(_middlePosText1, _endPosText1, _timeFraction2);
+        RateLaterButton.transform.position = Vector3.Lerp(_middlePosText2, _endPosText2, _timeFraction2);
+        _timeFraction2 += (Time.deltaTime / _duration);
+    }
+
+    private void HideNotification()
+    {
+        _isHidden = false;
+    }
+}
